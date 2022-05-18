36f42cdb0826c49b0f1cb3326c7efa6b24e1d39e
claudiogocella <104107475+claudiogocella@users.noreply.github.com>
2022-05-18 20:04:08 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+
+let paso1: string = "este es mi paso uno";
+let paso2: string = "este es mi segundo paso";
+
+console.log("los pasos para subir un posteo en Facebook");
+console.log(paso1);
+console.log(paso2);
